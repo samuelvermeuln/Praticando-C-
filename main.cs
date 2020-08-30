a045afd009672e1865fa4bd3e2a3724714502374
samuel vermeuln <vermeuln@ucl.br>
2020-08-30 19:19:50 +0000

    final do Exemplo 3 começo do Exemplo 4

--- main.cs
+++ main.cs
@@ -1,7 +1,33 @@
 using System;
+// 3. Faça um Programa que peça dois números e imprima a soma.
+// 4. Faça um Programa que peça as 4 notas bimestrais e mostre a média.
 
 class MainClass {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+
+  public static void ex3 () {
+    Console.WriteLine ("SOMA DE DOIS VALORES");
+    double num1, num2;
+    string a, b;
+    double result;
+
+    Console.WriteLine ("Primeiro valor: ");
+    a = Console.ReadLine();
+    num1 = double.Parse(a);
+
+    Console.WriteLine ("Segundo valor: ");
+    b = Console.ReadLine();
+    num2 = double.Parse(b);
+
+    result = num1 + num2;
+    Console.WriteLine ("Resultado da Soma: " + result);    
+    
+  }
+
+  public static void ex4 () {     
+    
+  }
+
+  public static void Main () {
+    ex3();
   }
 }
